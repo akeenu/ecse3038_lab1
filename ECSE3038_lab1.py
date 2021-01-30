899812fc46fe35c067b3c4d44e6b63127c85bdcb
akeenu <78240417+akeenu@users.noreply.github.com>
2021-01-29 23:55:18 -0500

    validate password function

--- ECSE3038_lab1.py
+++ ECSE3038_lab1.py
@@ -4,5 +4,14 @@ def hello():
 
 print(hello())
 
+#Question2
+def validatePassword(password):
+	if len(password) >= 8 and password.isalnum():
+		return True
+	else:
+		return False
 
+print(validatePassword("abc123"))
+print(validatePassword("password"))
+print(validatePassword("GreetingsFellowKids"))
 
